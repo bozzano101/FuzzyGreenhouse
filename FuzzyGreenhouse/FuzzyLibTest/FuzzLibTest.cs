9bb1e90c94e8809659a9672ddf5529e0b68e2115
Boris Karanovic <boskonet@gmail.com>
2021-10-29 19:00:53 +0200

    Fuzzy lib successfuly tested
    Fuzzy lib testing completed. Example for cars fuel usage and reliability done.

---
+++ FuzzyGreenhouse/FuzzyLibTest/FuzzLibTest.cs
@@ -0,0 +1,49 @@
+﻿using FuzzyLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FuzzyLibTest
+{
+    [TestClass]
+    public class FuzzLibTest
+    {
+        [TestMethod]
+        public void FuzzLibExampleTest()
+        {
+            List<FuzzyInput> potrosnja = new List<FuzzyInput>();
+            potrosnja.Add(new FuzzyInput("mala", new List<float> { 3, 10 }, new List<float> { 1, 0 }, 9));
+            potrosnja.Add(new FuzzyInput("srednja", new List<float> { 7, 10, 12, 15 }, new List<float> { 0, 1, 1, 0 }, 9));
+            potrosnja.Add(new FuzzyInput("velika", new List<float> { 12, 15}, new List<float> { 0, 1 }, 9));
+
+            List<FuzzyInput> pouzdanost = new List<FuzzyInput>();
+            pouzdanost.Add(new FuzzyInput("visoka", new List<float> { 5, 10 }, new List<float> { 1, 0 }, 8));
+            pouzdanost.Add(new FuzzyInput("niska", new List<float> { 8, 15 }, new List<float> { 0, 1}, 8));
+
+            List<FuzzyOutput> vrednost = new List<FuzzyOutput>();
+            vrednost.Add(new FuzzyOutput("mala", new List<float> { 7, 15 }, new List<float> { 1, 0 }));
+            vrednost.Add(new FuzzyOutput("srednja", new List<float> { 7, 15, 25, 40 }, new List<float> { 0, 1, 1, 0 }));
+            vrednost.Add(new FuzzyOutput("velika", new List<float> { 25, 40 }, new List<float> { 0, 1}));
+
+            List<FuzzyRules> rules = new List<FuzzyRules>();
+            rules.Add(new FuzzyRules(potrosnja[0], pouzdanost[0], vrednost[2], LogicOperator.AND));
+            rules.Add(new FuzzyRules(potrosnja[0], pouzdanost[1], vrednost[1], LogicOperator.AND));
+            rules.Add(new FuzzyRules(potrosnja[1], pouzdanost[0], vrednost[1], LogicOperator.AND));
+            rules.Add(new FuzzyRules(potrosnja[1], pouzdanost[1], vrednost[1], LogicOperator.AND));
+            rules.Add(new FuzzyRules(potrosnja[2], pouzdanost[0], vrednost[1], LogicOperator.AND));
+            rules.Add(new FuzzyRules(potrosnja[2], pouzdanost[1], vrednost[0], LogicOperator.AND));
+
+            float up = 0, down = 0;
+            foreach (var v in vrednost)
+            {
+                up += v.Mu * v.Centroid;
+                down += v.Mu;
+            }
+
+            Debug.WriteLine(up / down);
+
+            Assert.IsTrue(Math.Abs(up / down - 25.26315) < 0.001);
+        }
+    }
+}
