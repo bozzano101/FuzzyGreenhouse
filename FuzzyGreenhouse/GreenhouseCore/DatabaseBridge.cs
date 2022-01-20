bbadcecdc7912cd6bf46db1d9448b4c60764f28a
Boris Karanovic <boskonet@gmail.com>
2022-01-20 21:58:38 +0100

    Added MySqlConnector and made a test query against database

---
+++ FuzzyGreenhouse/GreenhouseCore/DatabaseBridge.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace GreenhouseCore
+{
+    class DatabaseBridge
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        private string ConnectionString { get; set; }
+
+        public DatabaseBridge(string server, string database, string userName, string password)
+        {
+            Server = server;
+            Database = database;
+            Username = userName;
+            Password = password;
+
+            ConnectionString = $"Server={server}; Database={database}; Uid={userName}; Pwd={password}";
+        }
+
+        public void ConnectToDatabase() {
+            var connection = new MySqlConnection(ConnectionString);
+            
+            try
+            {
+                connection.Open();
+
+                var command = new MySqlCommand("SELECT * FROM fuzzygreenhouse.set;", connection);
+                var result = command.ExecuteReader();
+                while(result.Read())
+                {
+                    Console.WriteLine($"{result[0]} - {result[1]}");
+                }
+
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ FuzzyGreenhouse/GreenhouseCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreenhouseCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbBridge = new DatabaseBridge("localhost","fuzzygreenhouse","bozzano101","Oyoneoyone1304");
+            dbBridge.ConnectToDatabase();
+            Console.ReadLine();
+        }
+    }
+}
