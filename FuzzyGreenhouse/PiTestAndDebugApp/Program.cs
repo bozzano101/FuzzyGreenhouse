891187cedf10dcd079c7fe018b645cebddbd6683
Boris Karanovic <boskonet@gmail.com>
2022-09-09 12:57:36 +0200

    Updated visual studio and assets, tested how remote debugging works on PI

---
+++ FuzzyGreenhouse/PiTestAndDebugApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Diagnostics;
+
+namespace PiTestAndDebugApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugger.Break();
+            int x = 2 + 3;
+            Console.WriteLine("hello world");
+            Console.WriteLine($"{x}");
+        }
+    }
+}
